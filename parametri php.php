1657a9a7440a79708b6864b3ff321e90dcac28bb
DarioCozzuto <77857380+DarioCozzuto@users.noreply.github.com>
2021-02-20 20:15:52 +0100

    Add files via upload

---
+++ parametri php.php
@@ -0,0 +1,12 @@
+<?php
+function data ($giorno, $mese, $anno) {
+        echo $giorno."/".$mese."/".$anno;
+    }
+    data('10','11','2012');
+?>
+<!DOCTYPE html>
+<html>
+<title>Parametri php</title>
+<body>
+</body>
+</html> 
